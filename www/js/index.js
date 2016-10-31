43e74c1487f22e46e1ad957cd491a2f62fc2e62f
hanxiumin <hanxiumin@xyzhdata.com>
2016-10-31 17:30:24 +0800

    vue

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,15 @@
-require(['jquery',"vender/a"],function(){
- console.log($);
+require(['jquery',"vender/a",'test'],function(){
+
  // bootstrap 依赖jquery
  require(['bootstrap']);
- console.log('a.js is finished');
+ // 首页nav 导航切换
+ $('#content .nav li').on('click',function(e){
+    var $index = $(this).index();
+    var active = $('#content .nav li.active').index();
+    if($index != active) {
+     $('#content .nav li.active').removeClass('active');
+     $(this).addClass('active');
+    }
+ });
+
 });
